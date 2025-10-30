dde8dda6f059dafe311f9e8992dfa8ecef95fb2b
Pankaj Kumar Rajbhar <pk902124@gmail.com>
2025-10-30 19:54:13 +0530

    learing react with npm

--- REACT-APP/index.jsx
+++ REACT-APP/index.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
 function Book({book}){
@@ -12,35 +11,35 @@ function Book({book}){
     );
 }
 
-function booklist(){
-    return(
-        <div className="book-list">
-            {booklist.map((b , index) =>(
-                <Book Key={index} book={b} />
-            ))}
-        </div>        
-    );
-}
 const books = [
     {
-        img: "",
+        img: "https://raajkart.com/media/catalog/product/cache/378cf9a83101843e5b8b1271b991c285/p/r/pradeep_fundamental_physics_class_11_set_of_12_volum.jpg",
         title: "PhysicsBook",
         price: "₹250"
     },
     {
-        img: "",
+        img: "https://res.cloudinary.com/bloomsbury-atlas/image/upload/w_568,c_scale,dpr_1.5/jackets/9781137610379.jpg",
         title: "ChemistryBook",
         price: "₹300"
     },
     {
-        img: "",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQZIDgkFVcrfJhxqJt_BU7wpfro9d5GolwzA&s",
         title: "MathsBook",
         price: "₹400"
     }
 ]
+function Booklist(){
+    return(
+        <div className="book-list">
+            {books.map((b , index) =>(
+                <Book key={index} book={b} />
+            ))}
+        </div>        
+    );
+}
 
 const parent = document.getElementById("root");
 const root = ReactDOM.createRoot(parent);
-root.render(<booklist book={books}/>)
+root.render(<Booklist book={books}/>)
 
 // ReactDOM.render(<BookList books={books} />, document.getElementById("root"));
---
+++ book-app/main.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+const parent = document.getElementById('root');
+const root = ReactDOM.createRoot(parent);
+root.render(<App />);
